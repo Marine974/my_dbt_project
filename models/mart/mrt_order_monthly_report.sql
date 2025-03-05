629419a1e023d67e0f7607085ecddeba1e56dc3b
peyre.marine@gmail.com <peyre.marine@gmail.com>
2025-03-05 10:30:25 +0000

    fix: Merge subqueries

--- models/mart/mrt_order_monthly_report.sql
+++ models/mart/mrt_order_monthly_report.sql
@@ -1,11 +1,11 @@
-WITH monthly_users_recap AS (
+WITH monthly_recap AS (
 
 
 SELECT DATE_TRUNC(order_date,month) AS order_month,
-COUNT(DISTINCT user_name) AS total_monthly_users
+COUNT(DISTINCT user_name) AS total_monthly_users,
+COUNT(order_id) AS total_monthly_orders
 FROM {{ source('sales_database', 'order')}}
 GROUP BY 1
-ORDER BY total_monthly_users DESC
 
 
 ), total_monthly_user_from_jawa_timur AS (
@@ -17,21 +17,13 @@ WHERE user.customer_state LIKE '%JAWA%TIMUR%'
 GROUP BY order_month
 
 
-), monthly_orders_recap AS (
-
-
-SELECT DATE_TRUNC(order_date,month) AS order_month,
-COUNT(order_id) AS total_monthly_orders
-FROM {{ source('sales_database', 'order')}}
-GROUP BY order_month
-
-
 )
-SELECT u.order_month,
-COALESCE(u.total_monthly_users,0) AS total_monthly_users,
+
+
+SELECT mr.order_month,
+COALESCE(mr.total_monthly_users,0) AS total_monthly_users,
 COALESCE(jt.total_monthly_users_from_jawa_timur,0) AS total_monthly_users_from_jawa_timur,
-COALESCE(o.total_monthly_orders,0) AS total_monthly_orders
-FROM monthly_users_recap AS u
-LEFT JOIN total_monthly_user_from_jawa_timur AS jt ON jt.order_month = u.order_month
-LEFT JOIN monthly_orders_recap AS o ON o.order_month = u.order_month
+COALESCE(mr.total_monthly_orders,0) AS total_monthly_orders
+FROM monthly_recap AS mr
+LEFT JOIN total_monthly_user_from_jawa_timur AS jt ON jt.order_month = mr.order_month
 ORDER BY order_month
